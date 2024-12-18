90e27b328d9e1c0d58d89cc26864c360d9503041
Sergi <sergi.prof@hotmail.com>
2024-12-18 16:12:35 +0100

    initial commit

---
+++ src/main/java/MarvelAPIFree/MarvelBackend/MarvelBackendApplication.java
@@ -0,0 +1,13 @@
+package MarvelAPIFree.MarvelBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelBackendApplication.class, args);
+	}
+
+}
