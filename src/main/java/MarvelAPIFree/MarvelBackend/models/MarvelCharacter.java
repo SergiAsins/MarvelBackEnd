5dda16063dcb41f1be7320a331598b295e479875
Sergi <sergi.prof@hotmail.com>
2024-12-19 12:50:33 +0100

    fix: crud works in testDBh2 but without lombok

--- src/main/java/MarvelAPIFree/MarvelBackend/models/MarvelCharacter.java
+++ src/main/java/MarvelAPIFree/MarvelBackend/models/MarvelCharacter.java
@@ -7,11 +7,6 @@ import lombok.*;
 import java.util.Objects;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-//@AllArgsConstructor
-@Builder
 public class MarvelCharacter {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,12 +16,6 @@ public class MarvelCharacter {
     private String description;
 
     public MarvelCharacter(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
-    public MarvelCharacter(Long id, String name, String description) {
-        this.id = id;
         this.name = name;
         this.description = description;
     }
@@ -52,6 +41,46 @@ public class MarvelCharacter {
                 ", description ='" + description + '\'' +
                 '}';
     }
+
+
+    // Default constructor
+    public MarvelCharacter() {
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    // Constructor with all parameters
+    public MarvelCharacter(Long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
+
+
 
 
